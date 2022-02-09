a6924e37a8681e54e7ab0acfcf0a0e372dfc8e21
aminov <alami@tshtt.uz>
2022-02-09 09:44:11 +0500

    v.2: add Data/*

---
+++ Infrastructure/Data/AccountRepository.cs
@@ -0,0 +1,30 @@
+﻿using Core.Interfaces;
+using Core.Models.Billing;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data
+{
+    public class AccountRepository : IAccountRepository
+    {
+        private readonly BillingContext _context;
+
+        public AccountRepository(BillingContext context)
+        {
+            this._context = context;
+        }
+        public async Task<IReadOnlyList<Account>> GetAccountAsync()
+        {
+            return await _context.Accounts.ToListAsync();
+        }
+
+        public async Task<Account> GetAccountByIdAsync(string id)
+        {
+            return await _context.Accounts.FindAsync(id);
+        }
+    }
+}
---
+++ Infrastructure/Data/ProductRepository.cs
@@ -0,0 +1,30 @@
+﻿using Core.Interfaces;
+using Core.Models.Billing;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data
+{
+    public  class ProductRepository : IProductRepository
+    {
+        private readonly BillingContext _context;
+
+        public ProductRepository(BillingContext context)
+        {
+            this._context = context;
+        }
+        public async Task<IReadOnlyList<Product>> GetProducstAsync()
+        {
+            return await _context.Products.ToListAsync();
+        }
+
+        public async Task<Product> GetProductByIdAsync(int id)
+        {
+            return await _context.Products.FindAsync(id); 
+        }
+    }
+}
